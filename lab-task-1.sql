05df1db0da275d5db0e5a1399a4b9e544bca555d
Rohan Verma <rv725555@gmail.com>
2023-05-20 21:09:21 +0530

    feat: Lab task 1.

---
+++ lab-task-1.sql
@@ -0,0 +1,270 @@
+DROP TABLE Journal;
+DROP TABLE Publisher;
+
+-- Q1A
+CREATE TABLE Publisher (
+Pub_ID NUMBER(4) PRIMARY KEY,
+Pub_Name VARCHAR2(25) DEFAULT 'Unknown',
+City VARCHAR2(15) DEFAULT 'Chennai',
+State VARCHAR2(15),
+Country VARCHAR2(25) DEFAULT 'India',
+URL VARCHAR2(25) UNIQUE
+);
+
+--
+INSERT INTO Publisher VALUES (1, 'Publisher A', 'Chennai', 'Tamil Nadu', 'India', 'www.publisherA.com');
+--
+INSERT INTO Publisher VALUES (2, 'Publisher B', 'Los Angeles', 'California', 'USA', 'www.publisherB.com');
+--
+INSERT INTO Publisher VALUES (3, 'Publisher C', 'Delhi', 'Delhi', 'India', 'www.publisherC.com');
+--
+INSERT INTO Publisher VALUES (4, 'Publisher D', 'Kolkata', 'West Bengal', 'India', 'www.publisherD.com');
+--
+INSERT INTO Publisher VALUES (5, 'Publisher E', 'Bengaluru', 'Karnataka', 'India', 'www.publisherE.com');
+
+
+-- Q1B
+CREATE TABLE Journal (
+Paper_ID NUMBER(5) PRIMARY KEY,
+Paper_Title VARCHAR2(25),
+Pub_ID NUMBER(4),
+Pub_Date DATE,
+Pub_Cost NUMBER(12, 2),
+Process_Cost NUMBER(12, 2) DEFAULT 0,
+Royalty NUMBER(3, 2) DEFAULT 0.12
+);
+
+--
+INSERT INTO Journal VALUES (1, 'Paper 1', 1, TO_DATE('2022-01-01', 'YYYY-MM-DD'), 1001, 200, 0.12);
+--
+INSERT INTO Journal VALUES (2, 'Paper 2', 2, TO_DATE('2022-02-01', 'YYYY-MM-DD'), 1500, 300, 0.12);
+--
+INSERT INTO Journal VALUES (3, 'Paper 3', 3, TO_DATE('2022-03-01', 'YYYY-MM-DD'), 2000, 400, 0.12);
+--
+INSERT INTO Journal VALUES (4, 'Paper 4', 4, TO_DATE('2022-04-01', 'YYYY-MM-DD'), 2500, 500, 0.12);
+--
+INSERT INTO Journal VALUES (5, 'Paper 5', 5, TO_DATE('2022-05-01', 'YYYY-MM-DD'), 3000, 600, 0.12);
+
+-- Q2a
+
+ALTER TABLE Journal
+ADD CONSTRAINT CHK_PublicationCost CHECK (Pub_Cost > 1000 AND Pub_Cost < 9999);
+
+-- Q2b
+
+ALTER TABLE Publisher
+ADD CONSTRAINT pname CHECK (Pub_Name <> 'XYZ');
+
+-- Q2c
+
+ALTER TABLE Journal
+ADD CONSTRAINT prname CHECK (Paper_Title <> 'PQR' AND Process_Cost >= 0);
+
+-- Q2d
+
+ALTER TABLE Publisher
+ADD Mobile_Number VARCHAR2(10);
+
+-- Q2e
+
+ALTER TABLE Journal
+ADD CONSTRAINT CHK_Royalty CHECK (Royalty >= 0 AND Royalty <= 1);
+
+-- Q2h
+
+ALTER TABLE Journal
+ADD CONSTRAINT FK_PubID FOREIGN KEY (Pub_ID)
+REFERENCES Publisher(Pub_ID)
+ON DELETE CASCADE;
+
+-- Q2i
+
+ALTER TABLE Journal
+RENAME COLUMN Process_Cost TO ProcessingCost;
+
+-- Q2j
+
+ALTER TABLE Journal
+ADD GST_Cost NUMBER(3, 2) DEFAULT 0.08;
+
+
+
+-- Q3a
+
+SELECT *
+FROM Publisher
+WHERE Pub_Name LIKE 'IEEE%';
+
+-- Q3b
+
+SELECT *
+FROM Publisher
+WHERE City = 'Chennai';
+
+-- Q3c
+
+SELECT Pub_Name, URL
+FROM Publisher
+WHERE Country <> 'India';
+
+-- Q3d - UPPER: Convert Pub_Name to uppercase
+SELECT UPPER(Pub_Name) FROM Publisher;
+
+-- Q3d - LOWER: Convert Pub_Name to lowercase
+SELECT LOWER(Pub_Name) FROM Publisher;
+
+-- Q3d - INITCAP: Convert Pub_Name to initial capital letters
+SELECT INITCAP(Pub_Name) FROM Publisher;
+
+-- Q3d - LENGTH: Get the length of Pub_Name
+SELECT LENGTH(Pub_Name) FROM Publisher;
+
+-- Q3d - LPAD: Left pad Pub_Name with '*' to a total length of 10 characters
+SELECT LPAD(Pub_Name, 10, '*') FROM Publisher;
+
+-- Q3d - RPAD: Right pad Pub_Name with '*' to a total length of 10 characters
+SELECT RPAD(Pub_Name, 10, '*') FROM Publisher;
+
+-- Q3d - LTRIM: Remove leading spaces from Pub_Name
+SELECT LTRIM(Pub_Name) FROM Publisher;
+
+-- Q3d - RTRIM: Remove trailing spaces from Pub_Name
+SELECT RTRIM(Pub_Name) FROM Publisher;
+
+-- Q3d - TRIM: Remove leading and trailing spaces from Pub_Name
+SELECT TRIM(Pub_Name) FROM Publisher;
+
+-- Q3d - NVL: Replace NULL Pub_Name with 'Not Available'
+SELECT NVL(Pub_Name, 'Not Available') FROM Publisher;
+
+-- Q3e
+
+SELECT *
+FROM Publisher
+ORDER BY Pub_Name;
+
+-- Q3f
+
+SELECT Pub_Name
+FROM Publisher
+WHERE Country = 'India' AND City = 'Mumbai';
+
+-- Q3g (SYSDATE gets the current date)
+
+SELECT *
+FROM Journal
+WHERE Pub_Date <= SYSDATE;
+
+-- Q3h
+
+SELECT Paper_ID, (ProcessingCost + Pub_Cost) * GST_Cost AS GST_Amount
+FROM Journal;
+
+-- Q3i
+
+DELETE FROM Publisher
+WHERE Pub_Name = 'RI Publisher';
+
+-- Q3j (Pub_ID 10 NOT in the Publisher table, so insertion will fail, maintaining referential integrity.)
+
+INSERT INTO Journal VALUES (5, 'New Paper', 10, SYSDATE, 1500, 100, 0.12, 0.08);
+
+
+-- Q3k
+
+UPDATE Publisher
+SET Country = 'New Zealand'
+WHERE Pub_Name = 'Publisher C';
+
+-- Q3l
+
+SELECT COUNT(*) AS Publisher_Count
+FROM Publisher;
+
+-- Q3m
+
+SELECT COUNT(*) AS Publisher_Count
+FROM Publisher
+WHERE Country = 'USA';
+
+-- Q3n
+
+SELECT Country, COUNT(*) AS Publisher_Count
+FROM Publisher
+GROUP BY Country
+ORDER BY Publisher_Count DESC;
+
+-- Q3p
+
+SELECT Country, COUNT(*) AS Publisher_Count
+FROM Publisher
+GROUP BY Country
+HAVING COUNT(*) >= 2;
+
+-- Q3q
+
+SELECT SUM(ProcessingCost) AS Total_Cost,
+AVG(ProcessingCost) AS Avg_Cost,
+MIN(ProcessingCost) AS Min_Cost,
+MAX(ProcessingCost) AS Max_Cost
+FROM Journal;
+
+-- Q3r
+
+SELECT EXTRACT(YEAR FROM Pub_Date) AS Year,
+SUM(Pub_Cost) AS Total_Amount
+FROM Journal
+GROUP BY EXTRACT(YEAR FROM Pub_Date);
+
+-- Q3s
+
+SELECT Pub_ID, AVG(Pub_Cost)
+FROM Journal
+GROUP BY Pub_ID;
+
+
+-- Q3t
+
+SELECT EXTRACT(YEAR FROM Pub_Date) AS Year,
+COUNT(*) AS Total_Papers,
+MAX(Pub_Cost) AS Max_Publication_Cost,
+MIN(Pub_Cost) AS Min_Publication_Cost
+FROM Journal
+GROUP BY EXTRACT(YEAR FROM Pub_Date);
+
+-- Q3u
+
+SELECT MAX(avg_costs.avg) as max_avg_publishing_cost
+FROM (
+  SELECT AVG(Pub_Cost) as avg
+  FROM Journal
+  GROUP BY Pub_ID 
+)avg_costs;
+
+-- Q3v
+
+SELECT Pub_ID
+FROM Journal
+GROUP BY Pub_ID
+HAVING COUNT(*) > 5;
+
+-- Q3w
+
+SELECT Paper_ID, Pub_Cost
+FROM Journal
+ORDER BY Pub_Cost;
+
+-- Q3x (MONTHS_BETWEEN used to calculate the difference in moths)
+
+SELECT Paper_ID, MONTHS_BETWEEN(SYSDATE, Pub_Date) AS Months_Difference
+FROM Journal;
+
+-- Q3y
+
+SELECT Paper_ID, TO_CHAR(Pub_Date, 'DD Mon YYYY HH:MI:SS') AS Pub_Date_Formatted
+FROM Journal;
+
+-- Q3z
+
+SELECT TO_CHAR(Pub_Date, 'YYYY-MM-DD HH24:MI:SS') AS Pub_Date_Formatted
+FROM Journal;
--- tables.md
+++ tables.md
@@ -1,4 +1,6 @@
-# Inlab 2 and 3A
+# Inlab
+
+## Inlab 2 and 3A
 
 | ID | ENAME  | DOJ |  SALARY | BONUS | DEPT | DESIGNATION | MANAGER | COMPID |
 |-|-|-|-|-|-|-|-|-|
@@ -8,7 +10,7 @@
 | 4 | Jack Abraham | 01-Jul-14 | 30000 | NULL | ETA | SSE | 2 | NULL |
 | 5 | Ayaz Mohammad | 01-Apr-14 | 40000 | NULL | ICP | TA | 1 | 1003 |
 
-# Inlab 3B
+## Inlab 3B
 
 | QuotationId | SupplierId | ItemCode |  QuotedPrice |
 |-|-|-|-|
@@ -54,7 +56,7 @@ F. For each of the item of every supplier , count the number of quotations recie
 13.  Write query displays departments that have more than 1 employees?
 14.  Write statement that shows the department ID, minimum salary, and maximum salary paid in that department, only if the minimum salary is less than 5000 and the maximum salary is more than 15000?
 
-# Inlab 4A
+## Inlab 4A
 
 Create the tables based on the conditions following them:
 
@@ -91,7 +93,7 @@ Sales Person table:
 11.  After the creation of sales table, add a constraint that the profit column cannot be null.
 12.  Alter a DEFAULT Constraint of vendor name after the Creation of a Table with “VIT,Chennai”
 
-# Inlab 4B
+## Inlab 4B
 
 13. Consider the following table and apply the concept of referential integrity over the tables using Foreign key. Check the options of ON DELETE/UPDATE CASCADE and ON DELETE/UPDATE SET NULL.
 
@@ -112,3 +114,67 @@ Orders table:
 | 2 | 44678 | 3 |
 | 3	| 22456 | 2 |
 | 4	| 24562 | 1 |
+
+# Lab Cycle Sheet
+
+# cycle sheet 1
+
+QUESTION
+The Publisher and their published article’s information about the Journal Data is given as below
+Publisher Table:
+| ColumnName | Type | Size | Constraint | Default |
+|-|-|-|-|-|
+| Pub_ID | Number | 4 | Primary Key | Auto Inc |
+| Pub_Name | Varchar2 | 25 | - | 'Unknown' |
+| City | Varchar2 | 15 | - | Chennai |
+| State | Varchar2 | 10 | - | - |
+| Country | Varchar2 | 25 | - | India | 
+| URL | Varchar2 | 25 | Unique | - |
+
+Journal Table:
+| ColumnName | Type | Size | Constraint | Default |
+|-|-|-|-|-|
+| Paper_ID | Number | 5 | Primary Key | Auto Inc |
+| Paper_Title | Varchar2 | 25 | - | - |
+| Pub_ID | Number | 4 | Foreign Key | - |
+| Pub_Date | Date | - | - | - |
+| Pub_Cost | Number | (12, 2) | - |
+| Process_Cost | Number | (12, 2) | - | 0 |
+| Royalty | Number | (3, 2) | - | 12% = 0.12  |
+
+1. Create tables identifying the primary keys and foreign keys, Insert necessary tuples into the tables. (Min 5 rows) Use Interactive way of inserting record.
+2. Enforce the Constraints
+a). The Publication Cost should be always greater than 1000 and less than 9999.
+b). Create a constraint with name pname so that it will check the publisher name should not be ‘XYZ’.
+c). Create a constraint with name prname so that it will check the paper title should not be ‘PQR’ and the processing cost should not be a negative value.
+d) Alter the table to add the mobile number of publisher table and its length should be 10.
+e)Check the value of royalty to the author and it should not be a negative ranges between 0 to 1
+h) Enforce foreign key constraints as per the tables given with 'ON CASECADE’ feature.
+i) Rename the column named as ‘Process_cost ’ into ‘ProcessingCost’
+j) add a column ‘GST_COST’ into Journal table such that it store the percentage of GST tax (0.15 for 15%). The default % of GST is 8%.
+
+3. Write SQL
+a) . List the publishers whose name starts with ‘IEEE’
+b). List the publishers from city ‘Chennai’.
+c). List the Foreign publisher and their URL
+d).Test the string manipulation functions – UPPER, LOWER, INITCAP, LENGTH, LPAD, RPAD, LTRIM, RTRIM and TRIM and NVL using select queries on data present in the tables. Use one query each for demonstration of one function.
+e) Sort the List of publisher alphabetically
+f) List out the publisher name from India and their city is ‘Mumbai’
+g) List the papers with its title published on or before today.
+h) Calculate the GST amount based on the GST % for all Total cost(processing Cost+Publication Cost).
+i) Delete the publisher with name ‘RI Publisher’j) Check the referential integrity during insertion / updation / Deletion of records of the table.
+k) Update the publisher’s Country column by new value by replacing the old value
+L) Count and Display the number of publishers.
+M) count the no of publishers from USA
+N) Count the No of Publisher from each country in descending order of the count
+P) List out the count of customers in each country whereas the country should have at least 2 publishers
+Q) List out the sum , avg, min max of all processingCost of the papers
+R). Find out the Total Amount of all papers for each year.
+S).Find out the average of publication cost of papers from each publisher.
+T).Apply the multiple functions into a group by clause using any one of the table
+U). Apply the Nesting Aggregate Functions
+V) List out the publisher id who have published the papers for more than one 5 times
+W) Display the paper-id sorted based on its amount
+X) Find out the months difference between sysdate and pubdate of every paper
+Y) Display the Pub_date of each paper in terms of its sentences
+Z) Display the pub date with YYYY-MM-DD HH24:MI:SS format
